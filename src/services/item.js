6dca9f7359560d64553594326a364250dde5dcad
Nataly Mora Mancera <nataly.mora@sofka.com.co>
2022-03-27 17:57:19 -0500

    adjusted item query

--- src/services/item.js
+++ src/services/item.js
@@ -2,20 +2,14 @@ import axios from 'axios';
 
 const baserUrl = ''
 
-export const getAllItems = async  () => {
-    const res = await axios.get()
-    return res.data;
+export const getAllItems = async  (search) => {
 
+    const data = await axios.post('http://localhost:4001/graph', {query: `query {getAllItemsType (search:"${search}") {id title price  { currency amount decimals} picture condition free_shipping sold_quantity}}`})
+    return (data);
 }
 
 
 export const getSearchItems = async(value) => {
     const res = await axios.get()
-    return res.data;
+
 }
-
-export const getItem = async(id) => {
-    const res = await axios.get()
-    return res.data;
-
-} 
